fe3708ef0a429f4509b4438aeb2bf557d4d53ee5
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2014-10-31 13:14:26 +0100

    Added another test script

---
+++ test-md-in-long.sh
@@ -0,0 +1,3 @@
+make && cat ../website/posts/2014-01-08-lezione-del-81-su-sistemi-operativi-memoria-processi.md \
+| ./index.js markdown input --template ../website/assets/layouts/post.jade --config ../website/site.json
+
